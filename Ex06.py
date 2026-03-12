0ffca0878473c77d78d2a486c78131bafd29c697
MateusCocco1 <mateus.cocco@aluno.impacta.edu.br>
2026-03-12 20:07:04 -0300

    Ex06.py

---
+++ Ex06.py
@@ -0,0 +1,10 @@
+ata = input("Digite a data (dd/mm/aaaa): ")
+
+dia, mes, ano = data.split("/")
+
+meses = [
+"Janeiro","Fevereiro","Março","Abril","Maio","Junho",
+"Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"
+]
+
+print(f"{dia} de {meses[int(mes)-1]} de {ano}")
